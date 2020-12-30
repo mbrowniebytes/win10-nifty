53d412f4286627cd6f7adf3c021555b2e8c36ee0
mbrowniebytes <mbrowniebytes@gmail.com>
2020-12-30 17:45:08 -0600

    install more apps comment some apps out that dont work currently

--- scripts/scoop.install.ps1
+++ scripts/scoop.install.ps1
@@ -18,6 +18,7 @@ $helpers.ScoopInstall("vcredist2010")
 $helpers.ScoopInstall("vcredist2012")
 $helpers.ScoopInstall("vcredist2013")
 $helpers.ScoopInstall("vcredist2019")
+# latest, which may match above
 $helpers.ScoopInstall("vcredist")
 
 $helpers.ScoopInstall("DirectX")
@@ -42,6 +43,9 @@ if ( !$helpers.IsDirectory($SCOOP_DIR + '/apps/concfg'))
     # concfg export <path>
 }
 
+# https://github.com/prezesp/scoop-viewer
+$helpers.ScoopInstall("scoop-viewer")
+
 # https://www.7-zip.org/
 $helpers.ScoopInstall("7zip")
 
@@ -51,13 +55,16 @@ $helpers.ScoopInstall("ProcessExplorer")
 # https://www.nirsoft.net/utils/cports.html
 $helpers.ScoopInstall("CurrPorts")
 
+# https://www.nirsoft.net/utils/multiple_ping_tool.html
+$helpers.ScoopInstall("PingInfoView")
+
 # https://www.nirsoft.net/utils/registry_changes_view.html
 $helpers.ScoopInstall("RegistryChangesView")
 
 
 $helpers.Echo("scoop system apps installed`n")
 
-exit 4
+
 
 #
 # dev
@@ -67,17 +74,50 @@ $helpers.Echo("scoop installing dev apps..`n")
 
 # fonts
 # https://github.com/matthewjberger/scoop-nerd-fonts/tree/master/bucket
-$helpers.Echo("scoop installing fonts..")
-scoop install FiraCode
-scoop install JetBrains-Mono
+# 1st install fails; 2nd install no errors but status complains about
+# $helpers.ScoopInstall("FiraCode")
+$helpers.ScoopInstall("JetBrains-Mono")
 
 # https://gitforwindows.org
-$helpers.Echo("scoop installing git..")
-scoop install git
+$helpers.ScoopInstall("git")
 
 # https://www.jetbrains.com/idea/
-$helpers.Echo("scoop installing IntelliJ..")
-scoop install IntelliJ-IDEA-Ultimate-portable
+$helpers.ScoopInstall("IntelliJ-IDEA-Ultimate-portable")
+
+# https://www.virtualbox.org/
+$helpers.ScoopInstall("virtualbox-np")
+
+# https://dbeaver.io/
+$helpers.ScoopInstall("dbeaver")
+
+# https://cmder.net/
+$helpers.ScoopInstall("cmder-full")
+
+# https://tools.stefankueng.com/grepWin.html
+$helpers.ScoopInstall("grepwin")
+
+# https://www.geany.org/
+$helpers.ScoopInstall("geany")
+
+# https://netbeans.org/
+$helpers.ScoopInstall("netbeans")
+
+# https://notepad-plus-plus.org/
+$helpers.ScoopInstall("notepadplusplus")
+
+# https://winmerge.org/
+$helpers.ScoopInstall("winmerge")
+
+# https://winscp.net/eng/index.php
+$helpers.ScoopInstall("winscp")
+
+# gui to edit hosts file
+# https://www.sordum.org/8266/
+$helpers.ScoopInstall("BlueLifeHostsEditor")
+
+# https://mremoteng.org/
+$helpers.ScoopInstall("mremoteng")
+
 
 $helpers.Echo("scoop dev apps installed`n")
 
@@ -87,47 +127,115 @@ $helpers.Echo("scoop dev apps installed`n")
 
 $helpers.Echo("scoop installing general apps..`n")
 
+# https://www.mozilla.org/en-US/firefox/
+$helpers.ScoopInstall("firefox")
+
 # https://www.google.com/chrome/
-$helpers.Echo("scoop installing googlechrome..")
-scoop install googlechrome
+$helpers.ScoopInstall("googlechrome")
+
+# https://vivaldi.com/
+$helpers.ScoopInstall("vivaldi")
+
+# https://audioswit.ch/er
+$helpers.ScoopInstall("audioswitcher")
 
 # https://www.videolan.org/
-$helpers.Echo("scoop installing vlc..")
-scoop install vlc
+$helpers.ScoopInstall("vlc")
 
 # Locate files and folders by name instantly
 # https://www.voidtools.com
-$helpers.Echo("scoop installing everything..")
-scoop install everything
+$helpers.ScoopInstall("everything")
 
-# switching between sound devices trivial
-# https://audioswit.ch/er
-$helpers.Echo("scoop installing audioswitcher..")
-scoop install audioswitcher
+# https://www.advancedrenamer.com/
+$helpers.ScoopInstall("advancedrenamer")
+
+# A simple open source portable app launcher
+# https://github.com/salvadorbs/Asuite
+$helpers.ScoopInstall("asuite")
+
+# https://www.ccleaner.com/ccleaner
+$helpers.ScoopInstall("ccleaner")
+
+# https://www.bleachbit.org/
+$helpers.ScoopInstall("bleachbit")
+
+# https://www.sordum.org/7952/dns-jumper-v2-2/
+$helpers.ScoopInstall("dnsjumper")
+
+# https://handbrake.fr/
+$helpers.ScoopInstall("handbrake")
+
+# https://keepass.info/
+$helpers.ScoopInstall("keepass")
+
+# https://www.libreoffice.org/
+$helpers.ScoopInstall("libreoffice-stable")
+
+# https://www.makemkv.com/
+$helpers.ScoopInstall("makemkv")
+
+# https://mkvtoolnix.download/
+$helpers.ScoopInstall("mkvtoolnix")
+
+# https://github.com/FiloSottile/mkcert
+$helpers.ScoopInstall("mkcert")
+
+# https://www.getpaint.net/
+$helpers.ScoopInstall("paint.net")
+
+# https://www.tracker-software.com/product/pdf-xchange-editor
+$helpers.ScoopInstall("pdf-xchange-editor")
+
+# https://www.qbittorrent.org/
+$helpers.ScoopInstall("qbittorrent-portable")
+
+# compacts browsers databases
+# https://crystalidea.com/speedyfox
+$helpers.ScoopInstall("speedyfox")
+
+# https://winaero.com/winaero-tweaker/
+$helpers.ScoopInstall("winaero-tweaker")
+
+# https://github.com/zadam/trilium
+$helpers.ScoopInstall("trilium")
+
+# https://www.thewindowsclub.com/fixwin-for-windows-10
+$helpers.ScoopInstall("FixWin10")
+
+# https://www.thewindowsclub.com/ultimate-windows-tweaker-4-windows-10
+$helpers.ScoopInstall("UltimateWindowsTweaker")
+
+# https://www.w10privacy.de/english-home
+# requires scoop 'upgrade'? shovel
+# https://github.com/Ash258/Scoop-Core
+# $helpers.ScoopInstall("W10Privacy")
+
+# https://discordapp.com/
+$helpers.ScoopInstall("discord")
 
 $helpers.Echo("scoop general apps installed`n")
 
 
 
 #
-# games
+# game apps
 #
 
-$helpers.Echo("scoop installing game apps..`n")
+# $helpers.Echo("scoop installing game apps..`n")
 
-# https://discordapp.com/
-$helpers.Echo("scoop installing discord..")
-scoop install discord
+# preference to instead install in C:\Games\
+# plus all the the other game services + games
 
 # https://store.steampowered.com/
-$helpers.Echo("scoop installing steam..")
-scoop install steam
+# $helpers.ScoopInstall("steam")
 
 # https://www.epicgames.com/store
-$helpers.Echo("scoop installing EpicGamesLauncher..")
-scoop install EpicGamesLauncher
+# $helpers.ScoopInstall("EpicGamesLauncher")
 
-$helpers.Echo("scoop game apps installed`n")
+# $helpers.Echo("scoop game apps installed`n")
+
+
+# placeholder for post install tweaks; will prob be another file
 
 # Windows 10 Debloater
 #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString
